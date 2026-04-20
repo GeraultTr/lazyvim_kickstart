06476bfb1cbea90b0763ccce0ae0e4a2abc4c8ba
Tristan Gérault <tristan.gerault@uclouvain.be>
2026-04-20 17:19:12 +0200

    claudecode bottom panel

---
+++ lua/plugins/claudecode.lua
@@ -0,0 +1,12 @@
+return {
+  "coder/claudecode.nvim",
+  opts = {
+    terminal = {
+      provider = "snacks",
+      snacks_win_opts = {
+        position = "bottom",   -- "bottom", "top", "left", "right", "float"
+        height = 0.35,         -- 35% of screen height
+      },
+    },
+  },
+}
